b66969200378bf8d692e9cde82be91bbd1004cc0
pb <paul.blanke1@web.de>
2018-03-09 15:48:59 +0100

    hLLO
    babab

--- src/testpackage/test.java
+++ src/testpackage/test.java
@@ -9,18 +9,5 @@ import de.datenbank.DBConnection;
 public class test {
 
 	public static void main(String[] args) {
-		Connection con = DBConnection.getConnection();
-		String sqlInsertStmt = "INSERT INTO nutzer VALUES (2, 'test','test', 'test' )";
-
-		try {
-			PreparedStatement stmt = con.prepareStatement(sqlInsertStmt);
-
-			stmt.execute();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-
-		}
-
-	}
+		Nutzer nutzer = new nutzer();
 }
