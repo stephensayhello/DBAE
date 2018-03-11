c0fde8ff8089b33565e37aca37fb9e46c17627be
Benjamin <benjamin.gajewski@yahoo.de>
2018-03-11 15:52:53 +0100

    emailklasse
    emailklasse is jetzt dirn, nocht nicht getestet

---
+++ src/de/utilities/mail.java
@@ -0,0 +1,54 @@
+package de.utilities;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Größtenteils von https://www.mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
+ * übernommen.
+ * Methoden und Übergabeparameter leicht verändert.
+ *
+ */
+public class mail {
+
+	private static final String USERNAME = "username@gmail.com";
+	private static final String PASSWORD = "password";
+
+	public static void SendMailTLS(String recipient, String subject, String text) {
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(USERNAME, PASSWORD);
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(USERNAME));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+			message.setSubject(subject);
+			message.setText(text);
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+}
