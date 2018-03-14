4ce9af25ea11b927219a211ecbd3ebe5df8d8bef
Benjamin <benjamin.gajewski@yahoo.de>
2018-03-14 19:58:12 +0100

    nixx

--- src/de/tags/ArtikelView.java
+++ src/de/tags/ArtikelView.java
@@ -10,9 +10,7 @@ public class ArtikelView extends TagSupport {
 	private static final long serialVersionUID = 1L;
 	private Produkt produkt;
 
-	public Produkt getProdukt() {
-		return produkt;
-	}
+	
 
 	public void setProdukt(Produkt produkt) {
 		this.produkt = produkt;
@@ -20,22 +18,22 @@ public class ArtikelView extends TagSupport {
 
 	public int doStartTag() {
 		try {
-			pageContext.getOut().append(getNavBar());
+			pageContext.getOut().append(getArtikelView());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return SKIP_BODY;
 	}
 
-	private String getNavBar() throws IOException {
-		String nav = ReadFromFile.readContentFromFile(pageContext, "artikel.html");
-		nav = nav.replace("PLATZHALTER1", produkt.getName());
-		nav = nav.replace("PLATZHALTER2", produkt.getBeschreibung());
-		nav = nav.replace("PLATZHALTER3", String.valueOf(produkt.getProdukt_id()));
-		nav = nav.replace("PLATZHALTER4", String.valueOf(produkt.getPreis()));
+	private String getArtikelView() throws IOException {
+		String artikelview = ReadFromFile.readContentFromFile(pageContext, "artikel.html");
+		artikelview = artikelview.replace("PLATZHALTER1", produkt.getName());
+		artikelview = artikelview.replace("PLATZHALTER2", produkt.getBeschreibung());
+		artikelview = artikelview.replace("PLATZHALTER3", String.valueOf(produkt.getProdukt_id()));
+		artikelview = artikelview.replace("PLATZHALTER4", String.valueOf(produkt.getPreis()));
 		
 
-		return nav;
+		return artikelview;
 	}
 
 }
