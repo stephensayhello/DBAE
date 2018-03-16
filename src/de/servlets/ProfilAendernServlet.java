265c3da9fe615f851abcb27571ebd06ae2251e3b
pb <paul.blanke1@web.de>
2018-03-16 18:34:49 +0100

    Gefixed: Einzelene Profiländerungen sind möglich.
    Es kann auch nur ein Wert des Profiles verändert werden.

--- src/de/servlets/ProfilAendernServlet.java
+++ src/de/servlets/ProfilAendernServlet.java
@@ -63,45 +63,177 @@ public class ProfilAendernServlet extends HttpServlet {
 		List<String> messages = new ArrayList<>();
 		session.removeAttribute("kundeeingeloggt");
 		
+		
 		if(vorname.equals("") && nachname.equals("") && email.equals("")  && strasse.equals("") 
 				&& hausnr.equals("") && ort.equals("")) {
 			// Mache Nichts keine Änderungen.
 			messages.add("Sie haben Ihre Informationen nicht verändert.");
 			
-		} else  {
+		} else if(strasse.equals("") && hausnr.equals("") && ort.equals("")) {
 			
 			messages.add("Ihre Informationen wurden erfolgreich geändert.");
-			Adresse adresse = kunde.getAdresse();
-			if(vorname != kunde.getVorname() && !vorname.equals("")) {
+	
+			if(vorname != kunde.getVorname() && !vorname.equals("") && nachname.equals("") && email.equals("")) {
 				
 				kunde.setVorname(vorname);
 				messages.add("Der Vorname wurde geändert.");
 				
-			}  else if(nachname != kunde.getNachname() && !nachname.equals("")) {
+			}  else if(nachname != kunde.getNachname() && !nachname.equals("") && vorname.equals("") && email.equals("")) {
 				
 				kunde.setNachname(nachname);
 				messages.add("Der Nachname wurde geändert.");
 				
-			} else if(email != kunde.getEmail() && Regex.pruefeRegexEMail(email)) {
+			} else if(email != kunde.getEmail() && Regex.pruefeRegexEMail(email) && !email.equals("")  && nachname.equals("") && vorname.equals("")) {
 				
 				kunde.setEmail(email);
 				messages.add("Die E-Mail wurde geändert.");
 			
-			} else if(strasse != adresse.getStrasse()) {
+			}  else if(vorname != kunde.getVorname() && nachname != kunde.getNachname() && email.equals("")) {
+				
+				kunde.setVorname(vorname);
+				kunde.setNachname(nachname);
+				messages.add("Der Vor und Nachname wurde geändert!");
+				
+			} else if(vorname != kunde.getVorname() && email != kunde.getEmail() && nachname.equals("") && Regex.pruefeRegexEMail(email)) {
+				kunde.setVorname(vorname);
+				kunde.setEmail(email);
+				messages.add("Der Vorname und die E-Mail wurden geändert!");
+				
+			}else if(nachname!= kunde.getNachname() && email != kunde.getEmail() && vorname.equals("") && Regex.pruefeRegexEMail(email)) {
+				
+				kunde.setNachname(nachname);
+				kunde.setEmail(email);
+				messages.add("Der Nachname und die E-Mail wurde geändert!");
+				
+			} else if(vorname != kunde.getVorname() && nachname != kunde.getNachname() && email != kunde.getEmail() && Regex.pruefeRegexEMail(email)) {
+				
+				kunde.setVorname(vorname);
+				kunde.setNachname(nachname);
+				kunde.setEmail(email);
+				messages.add("Der Vor- und Nachname und die EMail wurden aktualisiert");
+				
+			} else {
+				messages.add("Bei Aktualiserung ist leider ein technischer Fehler aufgetretten. Bitte geben Sie die Daten neu ein.");
+			}
+			
+			
+			
+			KundenOperations.kundenUpdateDaten(kunde);
+			session.setAttribute("kundeeingeloggt", kunde);
+			
+			
+			
+			
+			
+			
+			// Änderungen an der Adresse
+		} else if(vorname.equals("") && nachname.equals("") && email.equals("")) {
+			
+			messages.add(" Die Informationen Ihrer Adresse wurde aktualisiert.");
+			Adresse adresse = kunde.getAdresse();
+			
+			if(strasse != adresse.getStrasse() && !strasse.equals("") && hausnr.equals("") && ort.equals("") && plz == 0) {
 				adresse.setStrasse(strasse);
 				messages.add("Die Straße wurde geändert.");
 				
-			} else if(hausnr != adresse.getHausnummer()) {
+			} else if(hausnr != adresse.getHausnummer() && !hausnr.equals("")&& strasse.equals("") && ort.equals("") && plz == 0) {
 				adresse.setHausnummer(hausnr);
 				messages.add("Die Hausnummer wurde geändert.");
-			} else if(plz != adresse.getPlz()) {
+				
+			} else if(plz != adresse.getPlz() && hausnr.equals("") && ort.equals("") && strasse.equals("")) {
 				adresse.setPlz(plz);
 				messages.add("Die PLZ wurde geändert.");
-			} else if(ort != adresse.getOrt()) {
+				
+			} else if(ort != adresse.getOrt() && !ort.equals("") && hausnr.equals("") && strasse.equals("") && plz == 0) {
 				adresse.setOrt(ort);
 				messages.add("Der Ort wurde geändert.");
+			// 2 und mehr Sachsen werden geändert.	
+			} else if(strasse != adresse.getStrasse() && !strasse.equals("") && hausnr != adresse.getHausnummer() && !hausnr.equals("")
+					& ort.equals("") && plz == 0) {
+				adresse.setStrasse(strasse);
+				adresse.setHausnummer(hausnr);
+				messages.add("Die Straße und Hausnummer wurden geändert.");
 				
+			} else if(strasse != adresse.getStrasse() && !strasse.equals("") &&hausnr.equals("") && ort!= adresse.getOrt() && !ort.equals("")
+					& hausnr.equals("") && plz == 0) {
+				adresse.setStrasse(strasse);
+				adresse.setOrt(ort);
+				messages.add("Die Straße und der Ort wurden geändert.");
+				
+			} else if(strasse != adresse.getStrasse() && !strasse.equals("") && plz != adresse.getPlz() && ort.equals("") && hausnr.equals("")) {
+				adresse.setStrasse(strasse);
+				adresse.setPlz(plz);
+				messages.add("Die Straße und die PLZ wurden geändert.");
+				
+			} else if(strasse != adresse.getStrasse() && !strasse.equals("") && hausnr.equals("") && plz== 0
+					& !ort.equals("") && ort != adresse.getOrt()) {
+				adresse.setStrasse(strasse);
+				adresse.setOrt(ort);
+				messages.add("Die Straße und Hausnummer wurden geändert.");
+				
+			}  else if(strasse != adresse.getStrasse() && !strasse.equals("") && hausnr != adresse.getHausnummer() && !hausnr.equals("")
+					& ort.equals("") && plz == 0) {
+				adresse.setStrasse(strasse);
+				adresse.setHausnummer(hausnr);
+				messages.add("Die Straße und Hausnummer wurden geändert.");
+				
+			} else if(strasse != adresse.getStrasse() && !strasse.equals("") &&hausnr.equals("") && ort!= adresse.getOrt() && !ort.equals("")
+					& hausnr.equals("") && plz == 0) {
+				adresse.setStrasse(strasse);
+				adresse.setOrt(ort);
+				messages.add("Die Straße und der Ort wurden geändert.");
+				
+			}  else if(hausnr != adresse.getHausnummer() && !hausnr.equals("") && plz != adresse.getPlz() && ort.equals("") && strasse.equals("")) {
+				adresse.setHausnummer(hausnr);
+				adresse.setPlz(plz);
+				messages.add("Die Hausnummer und PLZ wurden geändert.");
+				
+			} else if(hausnr != adresse.getHausnummer() && !hausnr.equals("") && plz == 0 && !ort.equals("") && ort != adresse.getOrt() && strasse.equals("")) {
+				adresse.setHausnummer(hausnr);
+				adresse.setOrt(ort);;
+				messages.add("Die Hausnummer und der Ort wurden geändert.");
+				
+			}   else if(plz != adresse.getPlz() && !ort.equals("") && ort != adresse.getOrt() && strasse.equals("") && hausnr.equals("")) {
+				adresse.setOrt(ort);;
+				adresse.setPlz(plz);
+				messages.add("Die PLZ und der Ort wurden geändert.");
+				 //
+			} else if(strasse != adresse.getStrasse() && !adresse.equals("") && hausnr != adresse.getHausnummer() && !hausnr.equals("") 
+					&& plz!= adresse.getPlz() && ort.equals("")) {
+				adresse.setStrasse(strasse);
+				adresse.setHausnummer(hausnr);
+				adresse.setPlz(plz);
+				messages.add("Die Straße, die Hausnummer und die PLZ wurden geändert");
+				
+			} else if(strasse != adresse.getStrasse() && !adresse.equals("") && hausnr != adresse.getHausnummer() && !hausnr.equals("") &&
+					ort != adresse.getOrt() && !ort.equals("") && plz == 0) {
+				adresse.setStrasse(strasse);
+				adresse.setHausnummer(hausnr);
+				adresse.setOrt(ort);
+				messages.add("Die Straße, die Hausnummer und der Ort wurden geändert");
+				
+			} else if(adresse.equals("") && hausnr != adresse.getHausnummer() && !hausnr.equals("") &&
+					ort != adresse.getOrt() && !ort.equals("") && plz != adresse.getPlz()) {
+			
+				adresse.setHausnummer(hausnr);
+				adresse.setOrt(ort);
+				adresse.setPlz(plz);
+				messages.add("Die PLZ, die Hausnummer und der Ort wurden geändert");
+			} else if(strasse != adresse.getStrasse() && !adresse.equals("") && hausnr != adresse.getHausnummer() && !hausnr.equals("") &&
+					ort != adresse.getOrt() && !ort.equals("") && plz != adresse.getPlz()) {
+				adresse.setStrasse(strasse);
+				adresse.setHausnummer(hausnr);
+				adresse.setOrt(ort);
+				adresse.setPlz(plz);
+				messages.add("Die Straße, die Hausnummer , die PLZ und der Ort wurden geändert");
 			}
+			
+			
+			
+			
+			
+			
+			
 			kunde.setAdresse(adresse);
 			KundenOperations.kundenUpdateDaten(kunde);
 			session.setAttribute("kundeeingeloggt", kunde);
