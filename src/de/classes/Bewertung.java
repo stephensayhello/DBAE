bb718dea32a9cb39bab1b027cf43bd9799da9cfe
pb <paul.blanke1@web.de>
2018-03-21 21:36:20 +0100

    bewertungoperations angefangen:)

---
+++ src/de/classes/Bewertung.java
@@ -0,0 +1,9 @@
+package de.classes;
+
+public class Bewertung {
+
+	public Bewertung() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/de/databaseOperations/BewertungsOperations.java
+++ src/de/databaseOperations/BewertungsOperations.java
@@ -1,5 +1,22 @@
 package de.databaseOperations;
 
-public class BewertungsOperations {
+import java.util.List;
 
+import de.classes.Bewertung;
+import de.classes.Kunde;
+
+public class BewertungsOperations {
+	
+	private final String BEWERTUNG_NACH_ID = "SELECT * FROM bewertung WHERE bstnr = ?, kundennr = ?;";
+	private final String BEWERTUNGEN = "SELECT * FROM bewertung;";
+	
+	
+	public static Bewertung holeBewertung(Kunde kunde) {
+		return null;
+	}
+	
+	
+	public static List<Bewertung> holeAlleBewetung() {
+		return null;
+	}
 }
